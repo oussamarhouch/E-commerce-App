bf2f5c9cc85c298ee54454e63967d8894044b149
oussamarhouch <oussamarhouch@gmail.com>
2023-01-23 17:23:12 +0100

    new commit

--- client/src/_codux/component-templates/default/new-component.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './new-component.module.scss';
-import classNames from 'classnames';
-export interface NewComponentProps {
-    className?: string;
-}
-
-export const NewComponent = ({ className }: NewComponentProps) => {
-    return <div className={classNames(styles.root, className)}>NewComponent</div>;
-};
